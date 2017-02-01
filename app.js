6a422928a7ebceb7105f3c4477c1197fe01813af
comfyfeel <2399457a@opayq.com>
2017-02-01 14:19:00 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+function video()
+{
+var random = Math.floor(Math.random() * 100) // random number between 0 and 99
+var iframe = document.getElementById('playlist')
+iframe.setAttribute('src', iframe.getAttribute('src').replace(/index=\d+/, 'index='+random))
+}
+
+var totalCount = 11;
+function ChangeIt()
+{
+var num = Math.ceil( Math.random() * totalCount );
+document.body.background = 'images/'+num+'.jpg';
+document.body.style.backgroundSize = "cover";// Background repeat
+}
