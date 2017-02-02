7fc1d0b1351b6da3ff04e416404ad67ed93d75fd
bryznasty <dory.desjardins@gmail.com>
2017-02-01 20:20:51 -0500

    version 0.5

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-video();
+function video()
+{
+var random = Math.floor(Math.random() * 100) // random number between 0 and 99
+var iframe = document.getElementById('playlist')
+iframe.setAttribute('src', iframe.getAttribute('src').replace(/index=\d+/, 'index='+random))
+}
+
+var totalCount = 12;
+function ChangeIt()
+{
+var num = Math.ceil( Math.random() * totalCount );
+document.body.background = 'images/'+num+'.jpg';
+document.body.style.backgroundSize = "cover";// Background repeat
+}
