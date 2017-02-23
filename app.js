0041b72acb2f644f964984dd77848aaae6a2cd19
comfyfeel <2399457a@opayq.com>
2017-02-23 10:32:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,13 +12,4 @@ var num = Math.ceil( Math.random() * totalCount );
 document.body.background = 'images/'+num+'.jpg';
 document.body.style.backgroundSize = "cover";// Background repeat
 }
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-92529337-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
